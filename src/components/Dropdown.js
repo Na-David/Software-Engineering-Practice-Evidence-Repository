16109957e0592afa7ad22be19378ac7898518455
David Na <hna206@naver.com>
2022-08-24 13:34:28 +1200

    submit form finalized

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -4,11 +4,13 @@ import SEPractices from "../dummydata/SEPractices"
   const optionItems = SEPractices.map((SEPractice) =>
                 <option key={SEPractice.practice}>{SEPractice.practice}</option>
             );
+            <option value="">Select an SE Practice </option>
   const Dropdown = () => {
     return (
         <div>
              <select>
                 {optionItems}
+
              </select>
          </div>
 
--- src/pages/Submit-Article.js
+++ src/pages/Submit-Article.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import SubmissionForm from "../components/SubmissionForm.js";
 
 class SubmitArticle extends Component{
     render() {
@@ -6,9 +7,9 @@ class SubmitArticle extends Component{
         <div>
             <h2>Submit Article</h2>
                 <p>This will be a form to allow submitters to submit an article for possible inclusion in our SEED repo.</p>
-                <p>Also the display table of evidence levels for different claims will show here for the SE practice selected</p>
-                <p>Also the display table of evidence levels for different claims will show here for the SE practice selected</p>
+                <SubmissionForm/>
         </div>
+
         );
     }
 } 
